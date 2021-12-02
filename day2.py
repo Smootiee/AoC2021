2e7827ee9e29c8381edee068619b97d97ca235e5
smootie <sea-back@hotmail.com>
2021-12-02 09:54:32 +0100

    work in progress not done yet time for skewl

---
+++ day2.py
@@ -0,0 +1,20 @@
+with open("files/day2.txt") as file:
+    data = [str(line) for line in file]
+
+horiz_pos = 0
+depth = 0
+
+
+for x in data:
+    x = x.rstrip("\n")
+    if x.startswith('forward'):
+        horiz_pos = int(horiz_pos) + int(x[-1])
+    elif x.startswith('depth'):
+        depth = int(depth) + int(x[-1])
+    elif x.startswith('up'):
+        depth = int(depth) - int(x[-1])
+
+print(horiz_pos, depth)
+result = horiz_pos * depth
+
+print(result)
