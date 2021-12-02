d78bf1e7dd30b41f3981b190d4fe9f3801c17d3d
Smootiee <sea-back@hotmail.com>
2021-12-02 10:50:51 +0100

    some optimization

--- day2.py
+++ day2.py
@@ -2,7 +2,7 @@ import time
 start_time = time.time()
 
 with open("files/day2.txt") as file:
-    data = [str(line) for line in file]
+    data = [str(line).rstrip() for line in file]
 
 horiz_pos = 0
 depth = 0
@@ -10,14 +10,13 @@ aim = 0
 
 
 for x in data:
-    x = x.rstrip("\n")
     if x.startswith('forward'):
-        horiz_pos = int(horiz_pos) + int(x[-1])
+        horiz_pos = horiz_pos + int(x[-1])
         aim = aim + (depth * int(x[-1]))
     elif x.startswith('down'):
-        depth = int(depth) + int(x[-1])
+        depth = depth + int(x[-1])
     elif x.startswith('up'):
-        depth = int(depth) - int(x[-1])
+        depth = depth - int(x[-1])
 
 result_one = horiz_pos * depth
 result_two = horiz_pos * aim
