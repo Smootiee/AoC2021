89d773aa2b84db3d4850468a8d006273db6db5d4
Smootiee <sea-back@hotmail.com>
2021-12-02 10:25:37 +0100

    both tasks completed

--- day2.py
+++ day2.py
@@ -3,18 +3,21 @@ with open("files/day2.txt") as file:
 
 horiz_pos = 0
 depth = 0
+aim = 0
 
 
 for x in data:
     x = x.rstrip("\n")
     if x.startswith('forward'):
         horiz_pos = int(horiz_pos) + int(x[-1])
+        aim = aim + (depth * int(x[-1]))
     elif x.startswith('down'):
         depth = int(depth) + int(x[-1])
     elif x.startswith('up'):
         depth = int(depth) - int(x[-1])
 
-print(horiz_pos, depth)
-result = horiz_pos * depth
+result_one = horiz_pos * depth
+result_two = horiz_pos * aim
 
-print(result)
+print("--- Part One:",result_one, " ---")
+print("--- Part Two:",result_two, " ---")
