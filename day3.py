8bded22ffeec2a3b330f7014326cf2a4e9ceb053
smootie <sea-back@hotmail.com>
2021-12-03 19:31:45 +0100

    ugly one liner part 2 done, execution time sucks ass...

--- day3.py
+++ day3.py
@@ -19,5 +19,14 @@ number_two = ''.join(map(str, binary_list))
 bin_reverse = ''.join('1' if x == '0' else '0' for x in number_two)
 
 power_consumption = int(bin_reverse, 2) * int(number_two, 2)
+oxygen_list = data
+co2_list = data
+
+
+for i in range(int(list_data[0], 2)):
+    oxygen_list = [oxy for oxy in oxygen_list if oxy[i] == ['0', '1'][sum(int(oxy[i]) for oxy in oxygen_list) * 2 >= len(oxygen_list)]] if len(oxygen_list) > 1 else oxygen_list
+    co2_list = [co2 for co2 in co2_list if co2[i] == ['1', '0'][sum(int(co2[i]) for co2 in co2_list) * 2 >= len(co2_list)]] if len(co2_list) > 1 else co2_list
+
 print("--- Part One:", int(bin_reverse, 2) * int(number_two, 2), " ---")
+print("--- Part Two:", int(oxygen_list[0], 2) * int(co2_list[0], 2), " ---")
 print("Execution time: {:.2f}".format((time.time() - start_time)* 1000), "ms")
